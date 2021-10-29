988ae6930b5391a07c577dd2e91f22da431db170
DEV-ojy <skary0604@naver.com>
2021-10-29 22:55:23 +0900

    [CREATE] TSA

---
+++ AI/Text Summarization with Attention mechanism/Text Summarization with Attention.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+embedding_dim = 128
+hidden_size = 256
+
+# 인코더
+encoder_inputs = Input(shape=(text_max_len,))
+
+# 인코더의 임베딩 층
+enc_emb = Embedding(src_vocab, embedding_dim)(encoder_inputs)
+
+# 인코더의 LSTM 1
+encoder_lstm1 = LSTM(hidden_size, return_sequences=True, return_state=True ,dropout = 0.4, recurrent_dropout = 0.4)
+encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
+
+# 인코더의 LSTM 2
+encoder_lstm2 = LSTM(hidden_size, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
+
+# 인코더의 LSTM 3
+encoder_lstm3 = LSTM(hidden_size, return_state=True, return_sequences=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_outputs, state_h, state_c= encoder_lstm3(encoder_output2)
